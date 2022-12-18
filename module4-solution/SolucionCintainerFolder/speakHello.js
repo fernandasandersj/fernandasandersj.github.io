73417fc81ac1318caf4a0b1a4f475414ea230c71
fernandasandersj <fernandasandersj@gmail.com>
2022-12-18 13:22:59 +0100

    parte 2 terminada

--- module4-solution/SolucionCintainerFolder/speakHello.js
+++ module4-solution/SolucionCintainerFolder/speakHello.js
@@ -5,14 +5,14 @@ var speakWord = "Hello";
 
 (function (name){
     console.log(speakWord + " " + name);
-})("Coursera");
+})("Coursera 2 ");
 
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
 
 var helloSpeaker = {}; 
-helloSpeaker.name= "Coursera3";
+helloSpeaker.name= "Coursera 3 ";
 helloSpeaker.sayHello = function () {
     console.log("Hello " + helloSpeaker.name);
 }
@@ -47,10 +47,6 @@ helloSpeaker.sayHello();
 (function (window) { 
 
     var speakWord = "Hello";
-
-    // STEP 4: Rewrite the 'speak' function such that it is attached to the
-    // helloSpeaker object instead of being a standalone function.
-    // See Lecture 52, part 2
 
     var helloSpeaker = {};
     helloSpeaker.name = "Coursera 5";
